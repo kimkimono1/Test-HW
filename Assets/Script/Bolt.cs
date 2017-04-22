02e40be3b8386a0de35c1fa8cd9d80a96a85d6d9
Karn Auntachai <kim-kimono@hotmail.com>
2017-04-22 16:34:16 +0700

    5.กระสุนวิ่ง

---
+++ Assets/Script/Bolt.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolt : MonoBehaviour {
+	public float speed;
+	public Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = transform.forward * speed;
+
+	}
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -23,7 +23,6 @@ public class PlayerController : MonoBehaviour {
 
 		rb.velocity = (movement * speed);
 
-	
 
 		rb.rotation = Quaternion.Euler (0.0f, 0.0f, (rb.velocity.x * -tilt));
 	}
