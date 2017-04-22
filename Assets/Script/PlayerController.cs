4f4e4228863851e9a21dbc0c6ea900ed8c9c6175
Karn Auntachai <kim-kimono@hotmail.com>
2017-04-22 16:06:33 +0700

    4.เคลื่อนที่ยานทะลุจอ

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	private Rigidbody rb;
+
+	public float speed;
+	public float tilt;
+
+	void Start() {
+
+		rb = GetComponent<Rigidbody>();
+
 	}
+
+	void FixedUpdate() {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.velocity = (movement * speed);
+
 	
-	// Update is called once per frame
-	void Update () {
-		
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, (rb.velocity.x * -tilt));
 	}
 }
